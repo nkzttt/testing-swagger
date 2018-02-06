2c07b08510a2600f33ab1dfb673043baa45b57f7
nakazato hiroki <b10214@pc-10832.cyberagent.co.jp>
2018-02-06 14:39:30 +0900

    update

---
+++ test/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Application = () => {
+  return <h1>Hello world!</h1>
+};
+ReactDOM.render(<Application />, document.getElementById('app'));
